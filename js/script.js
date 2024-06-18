d4bf57247d0a331fa8aebee9237ba6cf4427086d
philcurtis422 <philcurtis422@gmail.com>
2024-06-18 10:23:08 -0400

    finished console basics

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// A word or multiple words wrapped in quotes is called a sting value type
+// a number is called a number value type
+
+    var firstName = "Philip";
+
+    console.log("cool");
+
+    console.log(firstName);
+    
