9ffaff5e841a9b6622ded5c9d4cc8650220b6afc
philcurtis422 <philcurtis422@gmail.com>
2024-06-21 10:19:14 -0400

    finished functions basics

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 // a number is called a number value type
 
     //primative value types
-    var firstName = "Philip"; //string
-    var lastName = "Curtis";
+    //var firstName = "Philip"; //string
+    //var lastName = "Curtis";
     var age = 30; //number
     var isADev = true; //boolean
     var un = undefined; //undefined
@@ -16,21 +16,16 @@
 
     var users = ['Bob', 99, 'Ann', 35, 'Billy', 45];
 
-   
-    //print a count from one to 10
+   function myFirstfunction() {
+    console.log('function has been called');
+   }
 
-    for( var i = 1; i < 11; i++){
-        console.log(i);
-    }
+   myFirstfunction();
 
-    
-    for(var index = 0; index < names.length; index++){
-        console.log(names[index]);
+   function printNames (arr) {
+    for(const name of arr){
+        console.log(name);
     }
-    var count = 1;
+   };
 
-    while(count <= 10){
-        console.log(count);
-        
-        count++;
-    }
+   printNames(names);
