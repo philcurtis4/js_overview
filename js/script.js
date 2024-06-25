0bb5c5158f318637deb86067388fac00ba28b652
philcurtis422 <philcurtis422@gmail.com>
2024-06-25 14:22:57 -0400

    finished note app example

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ const noteHeader = document.querySelector('.note-header');
 const NoteContainer = document.querySelector('.container');
 
 
+function getNotes () {
+    const notes = JSON.parse(localStorage.getItem('notes')) || [];
+    return notes;
+}
+
 // A function that is called when the form is submitted
 
 function createNote (eventObj) {
     eventObj.preventDefault();
-    noteHeader.innerHTML = 'Your Notes:'
 
     //grab the note input element
 
@@ -35,10 +39,15 @@ function createNote (eventObj) {
     // get the year from the date object
 
     const year = dateObj.getFullYear();
-    
+
+    const objHours = dateObj.getHours();
+
+    const hour = objHours > 12 ? objHours - 12 : objHours;
+
+    const minutes = dateObj.getMinutes();
     //combine all of those in a string - 7/24/2024
 
-    const dateStr = `${month}/${date}/${year}`;
+    const dateStr = `${month}/${date}/${year} ${hour}:${minutes}`;
 
     //create an object value with the note text and the current date
     const noteObj = {
@@ -49,21 +58,63 @@ function createNote (eventObj) {
 
     //pull the old data from the database(localstoreage) or an empty array if not previous data has been stored
     
-    const notes = JSON.parse(localStorage.getItem('notes')) || [];
-    console.log(notes);
+    //if no data is stored it will be falsy and an empty array is returned. If there is data stored it returns that
+    const notes = getNotes(); //created a function above
 
     //push the notes Obj to the notes array
 
     notes.push(noteObj);
+    const jsonArray = JSON.stringify(notes);
 
     //save the notes array to the local storage
 
-    localStorage.setItem('notes', JSON.stringify(notes));
+    localStorage.setItem('notes', jsonArray);
+
+        // resets the input box to empty
+    noteInput.value = "";
+
+    outputNotes();
     
 }
+
+function outputNotes () {
+    //retrieve the raw notes array from localStorage
+
+    const notes = getNotes(); //created a function above
+
+    //convert the raw array from localStorage
+
+        //already handled in getNotes();
+
+    //Target our main container element
+
+    const container = document.querySelector('.container');
+    const notesHeader = document.querySelector('.note-header');
+
+    if (notes.length) {
+        notesHeader.innerText = 'Your Notes:';
+    }
+
+    container.innerHTML = '';
+    //loop over each object in the array and output an article element into our main container for each object
+
+    for (const noteObj of notes) {
+        container.insertAdjacentHTML('beforeend', `
+            <article class="note">
+                <p class="note-text">${noteObj.text}</p>
+                <p class="note-date">Added: ${noteObj.date}</p>
+            </article>
+            `);
+
+    } 
+}
+
 // Set up and nessessary event listeners that should be listening when page loads
 
+
+outputNotes();
 noteForm.addEventListener('submit', createNote);
+
 
 
 // localStorage.setItem('name', 'Phil');
