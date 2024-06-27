01102ca464642233d98cc0a55fd62345a905a1d1
philcurtis422 <philcurtis422@gmail.com>
2024-06-27 14:47:44 -0400

    finished keyboard events

--- js/script.js
+++ js/script.js
@@ -1,29 +1,13 @@
-//const els = document.querySelectorAll('ul li');
-const ul = document.querySelector('ul');
+function keyPressed(eventObj) {
+    console.log(eventObj.key);
 
-function doSomething(eventObj) {
-    const el = eventObj.target;
-
-    if(el.tagName === 'LI') {
-        eventObj.target.style.background = 'red';
-}
-    console.log(eventObj.target.innerText);
-    // els.forEach(function(li) {
-    //     li.style.background = '#555';
-    // })
-    eventObj.target.style.background = 'red';
-}
-
-// for (const el of els) {
-//     el.addEventListener('click', doSomething)
-// }
-
-ul.addEventListener('click', doSomething);
-//eventObj.target is the same as using querySelector grabbing the element
-
-
-const fruits = ['orange', 'apple', 'grape'];
-
-fruits.forEach(function (li, index) {
-    console.log(li, index);
-});
+    const h1 = document.querySelector('h1');
+    h1.innerText = `${eventObj.key} - ${eventObj.keyCode}`
+  
+    if (eventObj.keyCode === 13) {
+      console.log(eventObj.key);
+      console.log('You have pressed the enter key');
+    }
+  }
+  
+  document.addEventListener('keydown', keyPressed);
