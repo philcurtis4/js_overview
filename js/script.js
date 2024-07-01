37420b35bb098a635efb05dd9812b3f04bd9db47
philcurtis422 <philcurtis422@gmail.com>
2024-07-01 11:52:01 -0400

    finished server-side-requests basics

--- js/script.js
+++ js/script.js
@@ -1,28 +1,67 @@
-// const newLi = $('<li>'); //creates a new element
-// newLi.text('New List item');
 
-// //$('li.special').text('changed')
+const apiKey = '453e76b47d31a5d94e87e19b3143c6ec';
 
-// $('li.special').html('<a href="https://google.com">Go To Google</a>')
+const url = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=${apiKey}&units=imperial`
 
-// $('ul').append(newLi); // adds another list item at the end
+// fetch(url) //returns a promise object
+//   .then(function (responseObj) {
+//     const dataPromise = responseObj.json(); //parse the data for you also returns a promise with a then and catch
+
+//     dataPromise.then(function (data) {
+//       console.log(data);
+//     });
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+  // fetch(url) //returns a promise object
+  // .then(function (responseObj) {
+  //   const dataPromise = responseObj.json(); //parse the data for you also returns a promise with a then and catch
+
+  //   // dataPromise.then(function (data) {
+  //   //   console.log(data);
+  //   // });
+
+  //   return 'passed'; //is passed to the next then
+  // })
+  // .then(function(val){
+  //   console.log(val, 'passed to 2nd then');
+  //   return 'something else';
+  // })
+  // .then(function(something) {
+  //   console.log(something);
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
+
+  fetch(url) //returns a promise object
+  .then(function (responseObj) {
+    return responseObj.json(); //parse the data for you also returns a promise with a then and catch
+
+  })
+  .then(function(data){
+    const html = `
+    <h2>Temp: ${data.main.temp}</h2>
+    `;
+
+    const outputDiv = document.querySelector('.output');
+
+    outputDiv.innerHTML = html;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 
-const $saveBtn = $('#save-note');
+// const promiseObject = {
+//   then: function () {
+//     //when things go well
+//   },
 
-function createNote() {
-  //eventObj.preventDefault();
-  const $titleInput = $('#title-input');
-  const $textInput = $('#text-input');
-  const $dateInput = $('#date-input');
-  const dateStr = $dateInput.val();
-  const dateTime = dayjs(dateStr)
+//   catch: function () {
+//     //when things dont go well
+//   }
 
-  console.log(dateTime.format('MMM DD YYYY'));
-  // added the day js script to get this format
-}
-
-$saveBtn.on('click', createNote)
-
-$('#date-input').datepicker();
-
+// }
