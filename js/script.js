50732da0c6aeb7282ab4ccc1d9b08b687c69fae0
philcurtis422 <philcurtis422@gmail.com>
2024-07-01 12:47:37 -0400

    finished server request with user input

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
+const weatherBtn = document.querySelector('#weather-btn');
 
-const apiKey = '453e76b47d31a5d94e87e19b3143c6ec';
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=${apiKey}&units=imperial`
 
+function outputWeather () {
+  const cityInput = document.querySelector('#city-input');
+  const apiKey = '453e76b47d31a5d94e87e19b3143c6ec';
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=imperial`
+
+fetch(url) //returns a promise object
+  // The first then callback function will be put to the side until the server responds back
+  .then(function (responseObj) {
+    //passing the parsed json object to the next then
+    return responseObj.json(); //parse the data for you also returns a promise with a then and catch
+
+  })
+  .then(function(data){
+    // Once we have the data, we create some html utilizing that data in some way
+    const html = `
+    <h2>Temp: ${data.main.temp}</h2>
+    `;
+    //target an existing element in the DOM
+    const outputDiv = document.querySelector('.output');
+    //output our completed html into the DOM/Window
+    outputDiv.innerHTML = html;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+}
+
+weatherBtn.addEventListener('click', outputWeather);
 // fetch(url) //returns a promise object
 //   .then(function (responseObj) {
 //     const dataPromise = responseObj.json(); //parse the data for you also returns a promise with a then and catch
@@ -36,23 +65,11 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=${a
   //   console.log(error);
   // });
 
-  fetch(url) //returns a promise object
-  .then(function (responseObj) {
-    return responseObj.json(); //parse the data for you also returns a promise with a then and catch
 
-  })
-  .then(function(data){
-    const html = `
-    <h2>Temp: ${data.main.temp}</h2>
-    `;
+  //we make a fetch request to openweathermap for current weather data
+  //the url must match the servers criteria for a request
 
-    const outputDiv = document.querySelector('.output');
-
-    outputDiv.innerHTML = html;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  
 
 
 // const promiseObject = {
