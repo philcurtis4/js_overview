6ca5a3e970bab76f8692adaa8c79b2d59dea1b43
philcurtis422 <philcurtis422@gmail.com>
2024-07-01 14:50:21 -0400

    finished starwars request example

--- js/script.js
+++ js/script.js
@@ -1,84 +1,37 @@
-const weatherBtn = document.querySelector('#weather-btn');
+
+const charSelect = document.querySelector('#char-select');
+const searchBtn = document.querySelector('#search-btn');
+
+function getCharacters () {
+  const baseUrl = 'https://swapi.dev/api';
+  const option = charSelect.value; //element triggering event
+  const url = `${baseUrl}/${option}`;
 
 
 
-function outputWeather () {
-  const cityInput = document.querySelector('#city-input');
-  const apiKey = '453e76b47d31a5d94e87e19b3143c6ec';
-
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=imperial`
-
-fetch(url) //returns a promise object
-  // The first then callback function will be put to the side until the server responds back
-  .then(function (responseObj) {
-    //passing the parsed json object to the next then
-    return responseObj.json(); //parse the data for you also returns a promise with a then and catch
-
+  fetch(url)
+  .then(function(resObj) {
+    return resObj.json();
   })
-  .then(function(data){
-    // Once we have the data, we create some html utilizing that data in some way
-    const html = `
-    <h2>Temp: ${data.main.temp}</h2>
-    `;
-    //target an existing element in the DOM
+  .then(function(data) {
     const outputDiv = document.querySelector('.output');
-    //output our completed html into the DOM/Window
-    outputDiv.innerHTML = html;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
 
+    outputDiv.innerHTML = '';
+
+    data.results.forEach(function (char) {
+      outputDiv.insertAdjacentHTML('beforeend', `
+        <div>
+          <h3>${char.name}</h3>
+          <p>Birth Year: ${char.birth_year}</p>
+        </div>
+        `)
+    })
+  })
 }
 
-weatherBtn.addEventListener('click', outputWeather);
-// fetch(url) //returns a promise object
-//   .then(function (responseObj) {
-//     const dataPromise = responseObj.json(); //parse the data for you also returns a promise with a then and catch
-
-//     dataPromise.then(function (data) {
-//       console.log(data);
-//     });
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-
-  // fetch(url) //returns a promise object
-  // .then(function (responseObj) {
-  //   const dataPromise = responseObj.json(); //parse the data for you also returns a promise with a then and catch
-
-  //   // dataPromise.then(function (data) {
-  //   //   console.log(data);
-  //   // });
-
-  //   return 'passed'; //is passed to the next then
-  // })
-  // .then(function(val){
-  //   console.log(val, 'passed to 2nd then');
-  //   return 'something else';
-  // })
-  // .then(function(something) {
-  //   console.log(something);
-  // })
-  // .catch(function (error) {
-  //   console.log(error);
-  // });
-
-
-  //we make a fetch request to openweathermap for current weather data
-  //the url must match the servers criteria for a request
-
+function init () {
+  searchBtn.addEventListener('click', getCharacters);
   
+}
 
-
-// const promiseObject = {
-//   then: function () {
-//     //when things go well
-//   },
-
-//   catch: function () {
-//     //when things dont go well
-//   }
-
-// }
+init();
