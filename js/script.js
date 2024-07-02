d180a8ec968f037900bfa8abce0d48abaf5296ef
philcurtis422 <philcurtis422@gmail.com>
2024-07-02 10:32:02 -0400

    jquery starwars ex

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 
-const charSelect = document.querySelector('#char-select');
-const searchBtn = document.querySelector('#search-btn');
+const $charSelect = $('#char-select');
+const $searchBtn = $('#search-btn');
 
 function getCharacters () {
   const baseUrl = 'https://swapi.dev/api';
-  const option = charSelect.value; //element triggering event
+  const option = $charSelect.val(); //element triggering event
   const url = `${baseUrl}/${option}`;
 
 
 
-  fetch(url)
-  .then(function(resObj) {
-    return resObj.json();
-  })
-  .then(function(data) {
-    const outputDiv = document.querySelector('.output');
+  $.get(url)
 
-    outputDiv.innerHTML = '';
+  .then( function(data){
+    const $outputDiv = $('.output');
+
+    $outputDiv.empty();
 
     data.results.forEach(function (char) {
-      outputDiv.insertAdjacentHTML('beforeend', `
+      $outputDiv.append( `
         <div>
           <h3>${char.name}</h3>
           <p>Birth Year: ${char.birth_year}</p>
         </div>
-        `)
+        `);
     })
-  })
-}
+})
+  
+  }
+  
 
 function init () {
-  searchBtn.addEventListener('click', getCharacters);
+  $searchBtn.on('click', getCharacters);
   
 }
 
